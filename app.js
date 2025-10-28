510244ed66d994dc1703fc9cc622fe78e1ce886a
Temperance Yu <13795207467@163.com>
2025-10-28 22:19:08 +0800

    till 49:50 mind grenade

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // Modules
 const names = require("./4-names");
 const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
 
-sayHi("Temperance");
-sayHi(names.john);
-sayHi(names.peter);
+require("./7-mind-grenade");
+
+// sayHi("Temperance");
+// sayHi(names.john);
+// sayHi(names.peter);
